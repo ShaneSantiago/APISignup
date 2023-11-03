f5117b48b19be19f2fad4781535bb74a6494c1dd
Shane <shane.santiago1@hotmail.com>
2023-11-03 15:05:12 -0300

    Refazendo

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,39 +1,33 @@
 const express = require("express");
+
 const UserModel = require("../models/User");
+
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-  const { email, phone } = req.body;
+  const { email } = req.body;
+  const { phone } = req.body;
 
-  // Verifique a existência de e-mail e telefone em paralelo
-  const [emailExists, phoneExists] = await Promise.all([
-    UserModel.findOne({ email }),
-    UserModel.findOne({ phone }),
-  ]);
-
-  if (emailExists) {
+  if (await UserModel.findOne({ email })) {
     return res.status(400).json({
       error: true,
       message: "E-mail do usuário já foi cadastrado",
     });
   }
-
-  if (phoneExists) {
+  if (await UserModel.findOne({ phone })) {
     return res.status(400).json({
       error: true,
       message: "Telefone já cadastrado",
     });
   }
 
-  // Se nenhum e-mail ou telefone existir, crie o usuário
-  const newUser = new UserModel(req.body);
-  await newUser.save();
+  const User = await UserModel.create(req.body);
 
   console.log(req.body);
   return res.json({
     error: false,
     message: "Registrado com sucesso",
-    data: newUser,
+    data: User,
   });
 });
 
